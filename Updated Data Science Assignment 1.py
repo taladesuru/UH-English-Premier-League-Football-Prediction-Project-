fd4d617c7cbfa3029203f46f3efe3c197d376e54
taladesuru <138279624+taladesuru@users.noreply.github.com>
2023-11-10 23:35:33 +0000

    Update Updated Data Science Assignment 1.py

--- Updated Data Science Assignment 1.py
+++ Updated Data Science Assignment 1.py
@@ -1,31 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Read the data from the CSV file
-data = pd.read_csv(r'C:\Users\DELL\OneDrive\Desktop\Thomus Assignment\\10-1310-data-interim-equality-impact-he-funding-figure-4.csv', skiprows=2)
+# Reading the data from the CSV file source
+data = pd.read_csv(r'C:\\Users\\HP\\OneDrive\\Desktop\\Thomas Assignment\\10-1310-data-interim-equality-impact-he-funding-figure-4.csv', skiprows=2)
 
-# Extract the data for plotting
+# Extraction of the data for plotting on the graph
 deciles = data['Decile']
 men_income = data['Men']
 women_income = data['Women']
 
-# Create a figure and plot the bar chart for income by decile
+# Creation of a figure and plot the bar chart 
 plt.figure(figsize=(10, 6))
 
-# Plot bars for men and women
+# Plot barchart for men and women for income by decile
 plt.bar(deciles, men_income, width=0.4, label='Men', align='center', alpha=0.7)
 plt.bar([x + 0.4 for x in deciles], women_income, width=0.4, label='Women', align='center', alpha=0.7)
 
-# Set plot labels and title for the bar chart
+# Set the labels for the plot and title for the bar chart
 plt.xlabel('Decile')
 plt.ylabel('Income')
 plt.title('Income by Decile for Men and Women')
 plt.xticks([x + 0.2 for x in deciles], deciles)  # Adjust the x-tick positions
 
-# Add a legend
+# Addition of a legend
 plt.legend()
 
-# Show the bar chart
+# Show the barchart
 plt.tight_layout()
 plt.show()
 
@@ -46,7 +46,7 @@ plt.show()
 
 
 
-df = pd.read_csv('C:\\Users\\DELL\\OneDrive\\Desktop\\10-1309-data-interim-impact-assessment-he-funding-table-10.csv', skiprows=1, encoding='ISO-8859-1', dtype=str)
+df = pd.read_csv('C:\\Users\\HP\\OneDrive\\Desktop\\Thomas Assignment\\10-1309-data-interim-impact-assessment-he-funding-table-10.csv', skiprows=1, encoding='ISO-8859-1', dtype=str)
 # Convert 'Size of firm (number of employees)' column to strings# Filter out rows with valid categories
 valid_categories = ['Micro: 1-9 employees - Insourced', 'Micro: 1-9 employees - Outsourced',
                     'Small: 10-49 employees - Insourced', 'Small: 10-49 employees - Outsourced',
@@ -55,7 +55,7 @@ valid_categories = ['Micro: 1-9 employees - Insourced', 'Micro: 1-9 employees - 
 
 df_filtered = df[df['Size of firm (number of employees)'].isin(valid_categories)]
 
-# Create a line graph
+# Create a line plot
 plt.figure(figsize=(10, 6))
 
 # Plot a line graph
@@ -63,7 +63,7 @@ plt.plot(df_filtered['Size of firm (number of employees)'], df_filtered['No of e
 plt.plot(df_filtered['Size of firm (number of employees)'], df_filtered['Mean hourly cost of personnel officer in 2014**'], marker='o', label='Mean hourly cost of personnel officer in 2014')
 plt.plot(df_filtered['Size of firm (number of employees)'], df_filtered['Mean hourly cost of IT technician***'], marker='o', label='Mean hourly cost of IT technician***')
 
-# Set plot labels and title for the line graph
+# Set labels and title for the Line graph
 plt.xlabel('Number of Employees')
 plt.ylabel('In 2014 Numbers of enterprises employing graduates ')
 plt.title('Enterprises Employing Graduates in 2014 by Size of Firm')
@@ -74,7 +74,7 @@ plt.xticks(rotation=45)
 # Addition of a legend
 plt.legend()
 
-# Show the Line graph
+# Show the Line plot
 plt.tight_layout()
 plt.show()
 
